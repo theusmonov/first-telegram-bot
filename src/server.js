af4f4e73fbeeb0ac088d21fe21215309d3f6ed25
theusmon0v <asadbek.usmonoff2003@gmail.com>
2023-08-30 21:05:54 +0500

    Userlar istirohat bog'lari haqida malumot bilishi kerak bo'lgan bo'lim tugadi undan bo'limlar tanlangan lokatsiyalar chiqadi

--- src/server.js
+++ src/server.js
@@ -53,7 +53,7 @@ bot.on("message" , (msg) => {
         keyboard: [
           [{text: "Amir Temur xiyoboni"}, {text:"Yapon bog‘i"}],
           [{text: "Botanika bog‘i"}, {text: "G‘alaba bog‘i"}],
-          [{text: "Adiblar xiyoboni"}, {text: "Ekobog‘"}],
+          [{text: "Adiblar xiyoboni"}, {text: "Eco Park"}],
           [{text: "Navro‘z etnografik bog‘i"}, {text: "Yangi O‘zbekiston bog‘i"}],
           [{text: "MAGIC CITY istirohat va madaniyat bog'i"}, {text: "Orqaga ⬅️"}], 
         ], 
@@ -75,9 +75,26 @@ bot.on("message" , (msg) => {
 
 bot.on("message", async (msg) => {
       if(msg.text === "Amir Temur xiyoboni"){
-        await bot.sendPhoto(msg.from.id )
         await bot.sendLocation(msg.from.id, 41.31202411778365, 69.28006720249844)
+      } else if (msg.text === "Yapon bog‘i"){
+        await bot.sendLocation(msg.from.id, 41.33981932532805, 69.28241868122475)
+      } else if (msg.text === "Botanika bog‘i") {
+        await bot.sendLocation(msg.from.id, 41.347674357939496, 69.31464885191872)
+      } else if(msg.text === "G‘alaba bog‘i"){
+        await bot.sendLocation(msg.from.id, 41.354318068317994, 69.22297264299296 )
+      } else if (msg.text === "Adiblar xiyoboni"){
+        await bot.sendLocation(msg.from.id, 41.302578062073025, 69.23554547282222)
+      } else if(msg.text === "Eco Park"){
+        await bot.sendLocation(msg.from.id, 41.30994936610001, 69.29603052904604)
+      } else if(msg.text ==="Navro‘z etnografik bog‘i"){
+        await bot.sendLocation(msg.from.id, 41.32662629746737, 69.26603645980231)
+      } else if(msg.text === "Yangi O‘zbekiston bog‘i"){
+        await bot.sendLocation(msg.from.id, 41.327210322281346, 69.4380532114791)
+      } else if(msg.text === "MAGIC CITY istirohat va madaniyat bog'i"){
+        await bot.sendLocation(msg.from.id, 41.3042680317906, 69.24654193420099)
       }
 })
+
+
 
 
